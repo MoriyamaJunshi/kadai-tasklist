5b208ed3017b53d5ec6ebeff0bc24582b7a87c10
Ubuntu <ubuntu@ip-172-31-22-250.ap-northeast-1.compute.internal>
2023-11-04 13:23:06 +0000

    mod 1

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -31,10 +31,10 @@ class MessagesController < ApplicationController
     @message = Message.find(params[:id])
 
     if @message.update(message_params)
-      flash[:success] = 'Message は正常に更新されました'
+      flash[:success] = 'Task は正常に更新されました'
       redirect_to @message
     else
-      flash.now[:danger] = 'Message は更新されませんでした'
+      flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
     end
   end
@@ -43,7 +43,7 @@ class MessagesController < ApplicationController
     @message = Message.find(params[:id])
     @message.destroy
 
-    flash[:success] = 'Message は正常に削除されました'
+    flash[:success] = 'Task は正常に削除されました'
     redirect_to messages_url
   end
   
