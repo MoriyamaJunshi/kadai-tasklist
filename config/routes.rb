081ebab2117aea56d99a51a01e15142b8ef952df
Ubuntu <ubuntu@ip-172-31-22-250.ap-northeast-1.compute.internal>
2023-11-18 06:06:02 +0000

    third

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'toppages#index'
+  root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   resources :tasks
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:create]
 end
